1a5dae3d41d67b9278759e8612575230f46e6a27
desertcoqui <fdetres@my365.bellevue.edu>
2022-10-21 13:57:20 -0500

    person class

---
+++ week-1/ts-examples/person.class.ts
@@ -0,0 +1,21 @@
+// Title: Person 1.4
+// Author: Professor Krasso
+// Date: 21 Oct 2022
+// Modified By: Detres
+// Description: Angular CLI
+// Code Attribution: Code provided by Professor's walk through videos
+
+import { IPerson } from './person.interface';
+
+class Person implements IPerson {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+}
+
+let myName = new Person('Detres', 'Papo');
+console.log(`My name is ${myName.firstName} ${myName.lastName}`);
---
+++ week-1/ts-examples/person.interface.ts
@@ -0,0 +1,11 @@
+// Title: Person 1.4
+// Author: Professor Krasso
+// Date: 21 Oct 2022
+// Modified By: Detres
+// Description: Angular CLI
+// Code Attribution: Code provided by Professor's walk through videos
+
+export interface IPerson {
+  firstName: String;
+  lastName: String;
+}
