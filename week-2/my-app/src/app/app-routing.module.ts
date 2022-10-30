eb9b0646f63a45d15921bb7df822f171748c6bde
desertcoqui <fdetres@my365.bellevue.edu>
2022-10-30 08:00:34 -0500

    2.2 complete

--- week-2/my-app/src/app/app-routing.module.ts
+++ week-2/my-app/src/app/app-routing.module.ts
@@ -8,12 +8,15 @@
 //
 //
 
+
+//importing components
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { AboutComponent } from './about/about.component';
 
+//paths with routes arrays
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
