2c566137a17fe309392c76effedc7435ea5eec76
desertcoqui <fdetres@my365.bellevue.edu>
2022-10-30 07:46:47 -0500

    home component

--- week-2/my-app/src/app/app.component.ts
+++ week-2/my-app/src/app/app.component.ts
@@ -1,9 +1,19 @@
+// Title: Assignment 2.2 Navigation
+// Author: Prof Krasso
+// Date: 30 Oct 2022
+// Modified: Detres
+// Description: Assignment 2.2- Navigation
+// Attribution: Code Exercise/Videos provided by Prof.
+//
+//
+//
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'my-app';
--- week-2/my-app/src/app/app.module.ts
+++ week-2/my-app/src/app/app.module.ts
@@ -1,18 +1,26 @@
+// Title: Assignment 2.2 Navigation
+// Author: Prof Krasso
+// Date: 30 Oct 2022
+// Modified: Detres
+// Description: Assignment 2.2- Navigation
+// Attribution: Code Exercise/Videos provided by Prof.
+//
+//
+//
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HomeComponent, AboutComponent, ContactComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
