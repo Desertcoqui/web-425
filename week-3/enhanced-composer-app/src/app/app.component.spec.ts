de55081adb0c2e2bafb551b3ec6965ccb1b4981f
desertcoqui <fdetres@my365.bellevue.edu>
2022-11-06 11:17:22 -0600

    attributions

--- week-3/enhanced-composer-app/src/app/app.component.spec.ts
+++ week-3/enhanced-composer-app/src/app/app.component.spec.ts
@@ -1,3 +1,17 @@
+// <!-- // Title: Assignment 3.2 Passing Data to routes, Part 1
+// // Author: Prof Krasso
+// // Date: Nov 6 2022
+// // Modified: Detres
+// // Description: Assignment 3.2 Passing Data to routes, Part 1
+// //Week-3 exercises/videos
+// //https://www.npmjs.com/package/bootstrap-icons
+// //https://angular.io/api/router/RouterLink
+// //https://angular.io/tutorial/toh-pt5
+// https://stackoverflow.com/questions/41370760/difference-between-routerlink-and-routerlink
+// https://www.youtube.com/watch?v=2nuUKwyaCzU&t=2s
+// https://www.youtube.com/watch?time_continue=5&v=jBcIjOtctIY&feature=emb_logo
+// https://cyberactive.bellevue.edu/ultra/courses/_513884_1/cl/outline-->
+
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
@@ -5,12 +19,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -30,6 +40,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('enhanced-composer-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'enhanced-composer-app app is running!'
+    );
   });
 });
--- week-3/enhanced-composer-app/src/app/composer-list/composer-list.component.spec.ts
+++ week-3/enhanced-composer-app/src/app/composer-list/composer-list.component.spec.ts
@@ -1,3 +1,17 @@
+// <!-- // Title: Assignment 3.2 Passing Data to routes, Part 1
+// // Author: Prof Krasso
+// // Date: Nov 6 2022
+// // Modified: Detres
+// // Description: Assignment 3.2 Passing Data to routes, Part 1
+// //Week-3 exercises/videos
+// //https://www.npmjs.com/package/bootstrap-icons
+// //https://angular.io/api/router/RouterLink
+// //https://angular.io/tutorial/toh-pt5
+// https://stackoverflow.com/questions/41370760/difference-between-routerlink-and-routerlink
+// https://www.youtube.com/watch?v=2nuUKwyaCzU&t=2s
+// https://www.youtube.com/watch?time_continue=5&v=jBcIjOtctIY&feature=emb_logo
+// https://cyberactive.bellevue.edu/ultra/courses/_513884_1/cl/outline-->
+
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ComposerListComponent } from './composer-list.component';
