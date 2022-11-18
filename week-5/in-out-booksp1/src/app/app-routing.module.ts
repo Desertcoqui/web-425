7bbdabf55bd2c7c80d2ec50f5116abfa6ee14532
desertcoqui <fdetres@my365.bellevue.edu>
2022-11-18 13:19:12 -0500

    gobble gobble

--- week-5/in-out-booksp1/src/app/app-routing.module.ts
+++ week-5/in-out-booksp1/src/app/app-routing.module.ts
@@ -1,10 +1,43 @@
+// Title: Assignment 5.2 Navigation n layout
+// Author: Prof Krasso
+// Date: Nov 18 2022
+// Modified: Detres
+//Week-5 exercises/videos
+//https://www.npmjs.com/package/bootstrap-icons
+//https://angular.io/api/router/RouterLink
+//https://angular.io/tutorial/toh-pt5
+// https://stackoverflow.com/questions/41370760/difference-between-routerlink-and-routerlink
+//https://youtu.be/d6gJLs3ZZII
+//https://stackoverflow.com/questions/74114391/npm-install-err-code-eresolve-while-resolving-angular-user-idle3-0-0
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+//component paths
+const routes: Routes = [
+  {
+    path: '',
+    component: BookListComponent,
+  },
+  {
+    path: 'book-list',
+    component: BookListComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- week-5/in-out-booksp1/src/app/app.module.ts
+++ week-5/in-out-booksp1/src/app/app.module.ts
@@ -1,20 +1,50 @@
+// Title: Assignment 5.2 Navigation n layout
+// Author: Prof Krasso
+// Date: Nov 18 2022
+// Modified: Detres
+//Week-5 exercises/videos
+//https://www.npmjs.com/package/bootstrap-icons
+//https://angular.io/api/router/RouterLink
+//https://angular.io/tutorial/toh-pt5
+// https://stackoverflow.com/questions/41370760/difference-between-routerlink-and-routerlink
+//https://youtu.be/d6gJLs3ZZII
+//https://stackoverflow.com/questions/74114391/npm-install-err-code-eresolve-while-resolving-angular-user-idle3-0-0
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    BookListComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    MatListModule,
+    FlexLayoutModule,
+    MatIconModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
